aa698904e9926ecec987fa5823765c49373ceca8
Anupam Patra <anupampatrabbsr1995@gmail.com>
2021-03-13 10:52:59 +0530

    commited1

--- src/components/App/NameCard/NameCard.jsx
+++ src/components/App/NameCard/NameCard.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import "./NameCard.css";
 
-const nmaeCheapUrl =
+//using this url to check the availability of the domain name clicked on the namecard and other details
+const nameCheapUrl =
     "https://www.namecheap.com/domains/registration/results/?domain=";
 
 const NameCard = ({ suggestedName }) => {
     return (
-        <a className="card-link" href={`${nmaeCheapUrl}${suggestedName}`} target="_blank" rel="noreferrer">
+        <a className="card-link" href={`${nameCheapUrl}${suggestedName}`} target="_blank" rel="noreferrer">
             <div className="result-name-card">
                 <p className="result-name">{suggestedName}</p>
             </div>
